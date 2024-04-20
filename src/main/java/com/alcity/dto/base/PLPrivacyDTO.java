32c52ceea726042ca994bda56c658bd499965e29
hosseini.j <jhoseyniv@gmail.com>
2024-04-20 17:18:34 +0330

    Class Naming - Refactoring

---
+++ src/main/java/com/alcity/dto/base/PLPrivacyDTO.java
@@ -0,0 +1,10 @@
+package com.alcity.dto.base;
+
+public class PLPrivacyDTO extends BaseItemSetDTO{
+    public PLPrivacyDTO() {
+    }
+
+    public PLPrivacyDTO(Long id, String label, String value, Long version, String created, String updated) {
+        super(id, label, value, version, created, updated);
+    }
+}
