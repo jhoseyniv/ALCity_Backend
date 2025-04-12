623187f0b02c393d2aa2ccb715ad24301d014d0c
jhoseyniv <91113959+jhoseyniv@users.noreply.github.com>
2025-04-12 17:29:20 +0330

    change step positions and add images to journey road maps

---
+++ src/main/java/com/alcity/api/HomeController.java
@@ -0,0 +1,27 @@
+package com.alcity.api;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Tag(name = "Home Page of backend", description = "Home Page of backend")
+@CrossOrigin(origins = "*" ,maxAge = 3600)
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+    @GetMapping("/home")
+    public String   welcome() {
+        return "redirect:/home.html";
+    }
+    @RequestMapping("/in")
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+
+}
